a8c4842730234f5b240f06f6fb62b5b898b9f932
BuildTools <unconfigured@null.spigotmc.org>
2024-02-18 15:00:08 +0000

    Initial template

---
+++ src/main/java/link/irwin/java/cloudflarednsupdater/CloudflarednsupdaterApplication.java
@@ -0,0 +1,13 @@
+package link.irwin.java.cloudflarednsupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudflarednsupdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudflarednsupdaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/link/irwin/java/cloudflarednsupdater/CloudflarednsupdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package link.irwin.java.cloudflarednsupdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudflarednsupdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
